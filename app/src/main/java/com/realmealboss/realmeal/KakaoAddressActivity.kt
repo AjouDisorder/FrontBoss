f800ee583d8457c3b5850de94ce5dba82a789b5b
ohyr <ohyr96@ajou.ac.kr>
2020-05-07 16:33:55 +0900

    가게주소 입력 페이지 추가

---
+++ app/src/main/java/com/realmealboss/realmeal/KakaoAddressActivity.kt
@@ -0,0 +1,12 @@
+package com.realmealboss.realmeal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KakaoAddressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kakao_address)
+    }
+}
