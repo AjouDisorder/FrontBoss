a67aa9a89d78941cfc1ca7064a88f18f89b3f50c
ohyr <ohyr96@ajou.ac.kr>
2020-06-01 19:07:35 +0900

    사장님이 qr 코드 인식하는 버튼 구현

--- app/src/main/java/com/realmealboss/realmeal/Home/OrderActivity.kt
+++ app/src/main/java/com/realmealboss/realmeal/Home/OrderActivity.kt
@@ -1,13 +1,42 @@
 package com.realmealboss.realmeal.Home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import com.realmealboss.realmeal.R
+import kotlinx.android.synthetic.main.activity_order.*
+
 
 class OrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        qrScan.setOnClickListener{
+            IntentIntegrator(this).initiateScan()
+        }
     }
+
+    // Get the results:
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        val result =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
 }
