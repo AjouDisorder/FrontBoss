e9dac38b9353239501f1a346176ffb862034d141
baewoojin514 <48473015+baewoojin514@users.noreply.github.com>
2020-06-17 19:43:29 +0900

    send notofication (#2)

---
+++ app/src/main/java/com/realmealboss/realmeal/MyFirebaseMessagingService.kt
@@ -0,0 +1,37 @@
+package com.realmealboss.realmeal
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if(remoteMessage?.notification != null){
+
+        }
+
+
+    }
+    private fun sendNotification(title: String?, body: String){
+        val intent = Intent(this, JoinActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSound(defaultSound)
+            .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+
+    }
+}
