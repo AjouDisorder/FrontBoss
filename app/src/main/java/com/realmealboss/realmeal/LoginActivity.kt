39aedcb92a572fa3a260e1644436b76273ae19f5
baewoojin514 <48473015+baewoojin514@users.noreply.github.com>
2020-06-19 13:04:36 +0900

    correction for icon and token refresh (#13)
    * send notofication
    
    * correct send notification
    
    * app icon updated
    
    * push icon and login for token refresh updated
    
    * conflict correction
    
    * push icon and login for refresh token updated
    
    * app icon is updated
    
    * correction of icon and token refresh

--- app/src/main/java/com/realmealboss/realmeal/LoginActivity.kt
+++ app/src/main/java/com/realmealboss/realmeal/LoginActivity.kt
@@ -39,10 +39,12 @@ class LoginActivity : AppCompatActivity() {
 
         //event
         login_button.setOnClickListener{
+            println("123")
             loginUser(login_id.text.toString(),login_password.text.toString())
         }
 
         kakao_button.setOnClickListener {
+            //Toast.makeText(this,"Kakao",Toast.LENGTH_SHORT).show()
             val intent = Intent(this, KakaoInfoActivity::class.java)
             startActivity(intent)
         }
@@ -63,11 +65,12 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this,"비밀번호를 입력해 주세요",Toast.LENGTH_SHORT).show()
             return;
         }
-        var token = FirebaseInstanceId.getInstance().getToken().toString();
+        val token = FirebaseInstanceId.getInstance().getToken().toString();
 
         iMyService.loginBoss(id, password, token).enqueue(object : Callback<ResponseBInfo> {
             override fun onFailure(call: Call<ResponseBInfo>?, t: Throwable?) {
                 Toast.makeText(this@LoginActivity,"연결 오류!!",Toast.LENGTH_SHORT).show()
+                println("0")
             }
 
             override fun onResponse(
@@ -78,8 +81,10 @@ class LoginActivity : AppCompatActivity() {
                 if(response?.body()?.result.toString() == "login failed") {
                     Toast.makeText(this@LoginActivity,"로그인 정보가 없거나 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
                 }else{
+                    println("1")
                     BossData.setOid(response?.body()?._id.toString())
                     BossData.setBid(response?.body()?.bossId.toString())
+                    println("2")
                     val intent = Intent(this@LoginActivity, MartListActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -162,3 +167,4 @@ private fun redirectSignupActivity() {
 */
 }
 /////////////////////////
+
